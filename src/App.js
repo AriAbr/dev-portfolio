e4027b5f0d9b874d50a1ea0b4cc88f1d882637d4
Ari <ari.abramowitz1@gmail.com>
2019-10-30 14:38:54 -0400

    added welcom message, headshot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import headshot from './logo.svg';
+import './css/App.css';
+import './css/header.css';
 
 function App() {
   return (
     <div className="App">
+      <div className="main-header">
+        <img src="/headshot.jpg" alt="headshot" id="headshot" />
+        <div id="main-header-message">
+          <div id="main-header-welcome">
+            Hi, I'm Ari Abramowitz.
+          </div>
+          <div id="main-header-developer">
+            I'm a fullstack web developer.
+          </div>
+          <div id="main-header-please-scroll">
+            Scroll down to see my projects, resume, and contact info.
+          </div>
+        </div>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
