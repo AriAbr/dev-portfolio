e142085b52b4c64d26758aa2b0a91aed6ca5c938
Ari <ari.abramowitz1@gmail.com>
2019-10-31 16:34:39 -0400

    made condense-on-scroll animation for header

--- src/App.js
+++ src/App.js
@@ -3,38 +3,37 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import logo from './logo.svg';
 import headshot from './logo.svg';
 import './css/App.css';
-import './css/header.css';
+import './css/welcome.css';
 import './css/main-display.css';
 import Projects from './components/Projects';
 import Resume from './components/Resume';
 import Contact from './components/Contact';
+import Welcome from './components/Welcome';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="main-header">
-        <img src="/headshot.jpg" alt="headshot" id="headshot" />
-        <div id="main-header-message">
-          <div id="main-header-welcome">
-            Hi, I'm Ari Abramowitz.
-          </div>
-          <div id="main-header-developer">
-            I'm a fullstack web developer.
-          </div>
-          <div id="main-header-please-scroll">
-            Scroll down to see my projects, resume, and contact info.
-          </div>
-        </div>
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Welcome />
+        <header className="App-header">
+          <Router>
+            <Route exact path="/" component={Projects} />
+            <Route path="/resume" component={Resume} />
+            <Route path="/contact" component={Contact} />
+          </Router>
+        </header>
       </div>
-      <header className="App-header">
-        <Router>
-          <Route exact path="/" component={Projects} />
-          <Route path="/resume" component={Resume} />
-          <Route path="/contact" component={Contact} />
-        </Router>
-      </header>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+
+class Welcome extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        isCondensed: false,
+        scrollPosition: 0
+    };
+  }
+
+  componentDidMount(){
+    console.log('welcome mounted')
+    document.addEventListener("scroll", (e) => {
+      console.log(window.pageYOffset);
+      var vertScrollPosition = window.pageYOffset;
+      if(vertScrollPosition > 236){
+        vertScrollPosition = 236;
+      }
+      this.setState({ scrollPosition: vertScrollPosition })
+      // if(!this.state.isCondensed){
+      //   if(vertScrollPosition > 70){
+      //     this.setState({ isCondensed: true })
+      //   }
+      // }
+      // if (vertScrollPosition > 70 && !this.state.isCondensed) {
+      //   this.setState({ isCondensed: true })
+      // } else if (vertScrollPosition <=70 && this.state.isCondensed) {
+      //   this.setState({ isCondensed: false })
+      // }
+    })
+  }
+
+
+  render() {
+    console.log(70 - this.state.scrollPosition)
+    var welcomeMessageMarginTop = 54 - this.state.scrollPosition;
+    if(welcomeMessageMarginTop < 0){
+      welcomeMessageMarginTop = 0;
+    }
+    var welcomeMessageHeight = 300 - this.state.scrollPosition;
+    var welcomeMessageLeftMargin = 480 - (1.7*this.state.scrollPosition);
+    var welcomeMessageVertPadding = 27 - ((27/204)*this.state.scrollPosition);
+    if(welcomeMessageVertPadding < 0){
+      welcomeMessageVertPadding = 0;
+    }
+    var welcomeMessageOpacity = 1 - (this.state.scrollPosition/220);
+    var condensedTitleOpacity = (this.state.scrollPosition/15) - (220/15);
+
+    var headshotWidth = 400 - (this.state.scrollPosition*1.5)
+    var welcomeContent = <></>
+    if (!this.state.isCondensed) {
+      welcomeContent = <>
+          <div id="main-header" style={{height: `${welcomeMessageHeight}px`}}>
+            <div id="condensed-title"
+              style={{
+                opacity: `${condensedTitleOpacity}`,
+              }}
+            >
+              Ari Abramowitz, Fullstack Web Developer
+            </div>
+            <img src="/headshot.jpg" alt="headshot" id="headshot"
+              width={`${headshotWidth}px`}
+              style={{
+                borderWidth: `${10 - this.state.scrollPosition/30}px`,
+                left: `${30 - this.state.scrollPosition/20}px`,
+                top: `${25 - this.state.scrollPosition/20}px`,
+                zIndex: "1",
+              }}
+            />
+            <div id="main-header-message"
+              style={{
+                marginLeft: `${welcomeMessageLeftMargin}px`,
+                marginRight: `0px`,
+                marginTop: `0px`,
+                marginBottom: `0px`,
+                padding: `${welcomeMessageVertPadding}px 0px`,
+                height: `${welcomeMessageHeight - welcomeMessageVertPadding*2}px`,
+                opacity: `${welcomeMessageOpacity}`,
+                zIndex: "0",
+              }}
+            >
+              <div id="main-header-welcome">
+                Hi, I'm Ari Abramowitz.
+              </div>
+              <div id="main-header-developer">
+                I'm a fullstack web developer.
+              </div>
+              <div id="main-header-please-scroll">
+                Scroll down to see my projects, resume, and contact info.
+              </div>
+            </div>
+          </div>
+        </>
+    } else if (this.state.isCondensed) {
+      welcomeContent = <>
+          <div id="condensed-welcome-div" >CONDENSED WELCOME GOES HERE</div>
+        </>
+    }
+    return (
+      <div id="welcome-content">
+        {welcomeContent}
+      </div>
+    )
+
+  }
+}
+
+export default Welcome
