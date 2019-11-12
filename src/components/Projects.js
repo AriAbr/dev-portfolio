b1df3583a9a74c728a40b4b42202f5039abf515f
Ari <ari.abramowitz1@gmail.com>
2019-11-12 15:27:20 -0500

    added real project descriptions

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -30,7 +30,8 @@ class Projects extends Component {
                 <h2 className="project-title">Spotifake</h2>
                 <img src="/spotifake-screenshot.png" alt="spotifake-screenshot" id="spotifake-screenshot" className="project-preview" width="600px"/>
                 <p className="project-description">
-                  Spotifake is a simple, lightweight music player built on <b>React.js</b>. It is a frontend-only site that can serve as a starting point for a more robust music streaming application.
+                  Spotifake is a simple, lightweight music player built on <b>React.js</b>.
+                  It is a frontend-only site that can serve as a starting point for a more robust music streaming application.
                 </p>
                 <div className="project-links">
                   <a target="_blank" rel="noopener noreferrer" href="http://ariabr-spotifake.herokuapp.com/" className="project-link">
@@ -50,7 +51,11 @@ class Projects extends Component {
               <div className="project-info">
                 <h2 className="project-title">Slaq</h2>
                 <img src="/slaq-screenshot.png" alt="slaq-screenshot" id="slaq-screenshot" className="project-preview" width="600px"/>
-                <p className="project-description">Slaq description wiil go here. In this unit, we saw Batsheva approach David as he neared the end of his life in order to inform him that his son, Adoniyahu, was already acting as if he were the king. In pasuk 21, Batsheva warns.</p>
+                <p className="project-description">
+                  Slaq is a real-time chat application, loosely inspired by the popular messaging app, Slack.
+                  It is built with with <b>React.js</b> on the frontend and <b>Firebase</b> on the backend.
+                  You can sign in with a Google account, then create your own chat rooms and send messages.
+                </p>
                 <div className="project-links">
                   <a target="_blank" rel="noopener noreferrer" href="http://ariabr-slaq.herokuapp.com/" className="project-link">
                     Slaq
@@ -69,7 +74,12 @@ class Projects extends Component {
               <div className="project-info">
                 <h2 className="project-title">Tirred</h2>
                 <img src="/tirred-screenshot.png" alt="tirred-screenshot" id="tirred-screenshot" className="project-preview" width="600px"/>
-                <p className="project-description">Tirred is a Reddit-inspired, sleep-themed discussion board. In this unit, we saw Batsheva approach David as he neared the end of his life in order to inform him that his son, Adoniyahu, was already acting as if he were the king. In pasuk 21, Batsheva warns.</p>
+                <p className="project-description">
+                  Tirred is a Reddit-inspired, sleep-themed discussion board where you can post, comment, and vote on posts.
+                  The backend of the application is built on <b>Node.js</b>, <b>Express.js</b>, <b>Sequelize</b>, and <b>PostgreSQL</b>.
+                  The views are constructed with <b>EJS</b> and <b>Bootstrap</b>.
+                  The app uses <b>Passport.js</b> to handle user authentication.
+                </p>
                 <div className="project-links">
                   <a target="_blank" rel="noopener noreferrer" href="http://tirred.herokuapp.com/" className="project-link">
                     Tirred
@@ -88,7 +98,9 @@ class Projects extends Component {
               <div className="project-info">
                 <h2 className="project-title">Portfolio</h2>
                 <img src="/portfollio-screenshot.png" alt="portfolio-screenshot" id="portfolio-screenshot" className="project-preview" width="600px"/>
-                <p className="project-description">I built this site myself using <b>React.js</b>. You can check out the code here:</p>
+                <p className="project-description">
+                  I built this site myself using <b>React.js</b>. You can check out the code here:
+                </p>
                 <div className="project-links">
                   <a target="_blank" rel="noopener noreferrer" href="https://github.com/AriAbr/dev-portfolio" className="project-link">
                     GitHub
